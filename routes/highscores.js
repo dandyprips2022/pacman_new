4d64d8385130d017b78e38b2ac330a5d430b886c
dandyprips2022 <113714985+dandyprips2022@users.noreply.github.com>
2024-09-17 18:45:27 +0530

    Update highscores.js
    Added custom instrumentation

--- routes/highscores.js
+++ routes/highscores.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var Database = require('../lib/database');
+const { trace } = require('@opentelemetry/api');
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -40,56 +41,72 @@ router.get('/list', urlencodedParser, function(req, res, next) {
 
 // Accessed at /highscores
 router.post('/', urlencodedParser, function(req, res, next) {
-    console.log('[POST /highscores] body =', req.body,
-                ' host =', req.headers.host,
-                ' user-agent =', req.headers['user-agent'],
-                ' referer =', req.headers.referer);
+    // Create a tracer instance
+    const tracer = trace.getTracer('default');
+    
+    // Start a new span for the POST request
+    const span = tracer.startSpan('POST /highscores');
 
-    var userScore = parseInt(req.body.score, 10),
-        userLevel = parseInt(req.body.level, 10);
+    try {
+        console.log('[POST /highscores] body =', req.body,
+                    ' host =', req.headers.host,
+                    ' user-agent =', req.headers['user-agent'],
+                    ' referer =', req.headers.referer);
 
-    Database.getDb(req.app, function(err, db) {
-        if (err) {
-            return next(err);
-        }
+        var userScore = parseInt(req.body.score, 10),
+            userLevel = parseInt(req.body.level, 10);
 
-        // Insert high score with extra user data
-        db.collection('highscore').insertOne({
-                name: req.body.name,
-                cloud: req.body.cloud,
-                zone: req.body.zone,
-                host: req.body.host,
-                score: userScore,
-                level: userLevel,
-                date: Date(),
-                referer: req.headers.referer,
-                user_agent: req.headers['user-agent'],
-                hostname: req.hostname,
-                ip_addr: req.ip
-            }, {
-                w: 'majority',
-                j: true,
-                wtimeout: 10000
-            }, function(err, result) {
-                var returnStatus = '';
+        Database.getDb(req.app, function(err, db) {
+            if (err) {
+                span.setStatus({ code: 2 }); // 2 = ERROR
+                span.end();
+                return next(err);
+            }
 
-                if (err) {
-                    console.log(err);
-                    returnStatus = 'error';
-                } else {
-                    console.log('Successfully inserted highscore');
-                    returnStatus = 'success';
-                }
-
-                res.json({
+            // Insert high score with extra user data
+            db.collection('highscore').insertOne({
                     name: req.body.name,
+                    cloud: req.body.cloud,
                     zone: req.body.zone,
+                    host: req.body.host,
                     score: userScore,
                     level: userLevel,
-                    rs: returnStatus
+                    date: Date(),
+                    referer: req.headers.referer,
+                    user_agent: req.headers['user-agent'],
+                    hostname: req.hostname,
+                    ip_addr: req.ip
+                }, {
+                    w: 'majority',
+                    j: true,
+                    wtimeout: 10000
+                }, function(err, result) {
+                    span.end(); // End the span after the operation is complete
+                    var returnStatus = '';
+
+                    if (err) {
+                        console.log(err);
+                        returnStatus = 'error';
+                    } else {
+                        console.log('Successfully inserted highscore');
+                        returnStatus = 'success';
+                    }
+
+                    res.json({
+                        name: req.body.name,
+                        zone: req.body.zone,
+                        score: userScore,
+                        level: userLevel,
+                        rs: returnStatus
+                    });
                 });
-            });
-    });
+        });
+    } catch (error) {
+        span.setStatus({ code: 2 }); // 2 = ERROR
+        span.recordException(error);
+        span.end();
+        next(error);
+    }
 });
 
 module.exports = router;
